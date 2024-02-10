ed198c0e848c8139c92369dcff654191d6ef4507
yunhwaa <140477502+yunhwaa@users.noreply.github.com>
2024-02-10 18:35:03 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.py
@@ -0,0 +1,9 @@
+N = int(input())
+
+if N == 1:
+  print("*")
+else:
+  print(" " * (N-1) + "*")
+  for i in range(2, N):
+    print(" " * (N-i) + "*" + " " * (2*(i-1)-1) + "*")
+  print("*" * (2*N-1))
