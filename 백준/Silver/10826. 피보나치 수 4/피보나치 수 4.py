f58ef0a218288cb7d589b047f79e2944c318b804
yunhwaa <140477502+yunhwaa@users.noreply.github.com>
2024-02-16 23:33:51 +0900

    [Silver V] Title: 피보나치 수 4, Time: 76 ms, Memory: 34912 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+#작은 문제부터 해결해서 저장할 dp 테이블
+dp = [0] * 10001
+
+dp[0] = 0
+dp[1] = 1
+dp[2] = 1
+
+#피보나치 수열을 반복문으로 구현(bottom up)
+for i in range(3, n+1):
+  dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
