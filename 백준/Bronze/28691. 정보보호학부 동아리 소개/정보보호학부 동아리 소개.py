c441276db901d47933a14155505b5bc7acaf3f1f
yunhwaa <140477502+yunhwaa@users.noreply.github.com>
2024-02-22 03:26:10 +0900

    [Bronze V] Title: 정보보호학부 동아리 소개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28691. 정보보호학부 동아리 소개/정보보호학부 동아리 소개.py
@@ -0,0 +1,12 @@
+name = input()
+
+if name == "M":
+  print("MatKor")
+elif name == "W":
+  print("WiCys")
+elif name == "C":
+  print("CyKor")
+elif name == "A":
+  print("AlKor")
+elif name == "$":
+  print("$clear")
