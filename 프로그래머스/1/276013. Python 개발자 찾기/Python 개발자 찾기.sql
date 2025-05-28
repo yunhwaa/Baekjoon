d4f3eb86a174ca67c3079e0a530a669e15d88c3d
yunhwaa <140477502+yunhwaa@users.noreply.github.com>
2025-05-28 16:49:30 +0900

    [level 1] Title: Python 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
+++ 프로그래머스/1/276013. Python 개발자 찾기/Python 개발자 찾기.sql
@@ -1,5 +1,5 @@
 -- 코드를 작성해주세요
 SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
 FROM DEVELOPER_INFOS
-WHERE (SKILL_1 = 'Python') or (SKILL_2 = 'Python') or (SKILL_3 = 'Python')
-ORDER BY ID ASC;
+WHERE (SKILL_1 = 'Python' OR SKILL_2 = 'Python' OR SKILL_3 = 'Python')
+ORDER BY ID;
