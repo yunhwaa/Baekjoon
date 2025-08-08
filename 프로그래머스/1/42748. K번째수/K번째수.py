990a9a1a58bfc5f9a53fb5c1be30ae4d902c9471
yunhwaa <140477502+yunhwaa@users.noreply.github.com>
2025-08-08 19:16:55 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,11 +1,7 @@
 def solution(array, commands):
-    ans = []
-    l1 = []
-    list1 = []
-    
-    for i in commands:
-        l1 = i
-        list1 = sorted(array[l1[0] - 1:l1[1]])
-        ans.append(list1[l1[2] - 1])
-    
-    return ans
+    answer = []
+    for c in commands:
+        arr = array[c[0]-1:c[1]]
+        arr = sorted(arr)
+        answer.append(arr[c[2]-1])
+    return answer
