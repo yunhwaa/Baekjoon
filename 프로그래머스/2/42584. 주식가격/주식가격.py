c810883af75461217633596d7897c4025f443b38
yunhwaa <140477502+yunhwaa@users.noreply.github.com>
2025-08-10 20:18:20 +0900

    [level 2] Title: 주식가격, Time: 139.45 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,28 +1,15 @@
 def solution(prices):
-    ans = []
-    
-    for i in range(len(prices)):
-        cnt = 0
-        j = i + 1
-        
-        while True:
-            if j == len(prices):
-                ans.append(cnt) 
-                break
-            elif prices[j] >= prices[i]:
-                cnt += 1
-                j += 1
-            else:
-                cnt += 1
-                ans.append(cnt)
-                break
-        
-    return ans
-            
-        
-# i랑 j가 순환
-# 1꺼냄 -> prices 길이만큼 비교 -> 중간에 더 작은 수가 나오면 break -> 그렇지 않으면 cnt 1씩 증가 
+    answer = []
+    price = 0
 
-# i는 prices길이보다 1작은 곳까지 or i가 prices 길이 값이면 0 추가 
-# 넣고나면 cnt 다시 0으로 초가 
-
+    for p in range(len(prices)):
+        price = prices[p]
+        time = 0
+        for i in range(p+1, len(prices)):
+            if prices[i] >= price:
+                time += 1
+            elif prices[i] < price:
+                time += 1
+                break
+        answer.append(time)
+    return answer
