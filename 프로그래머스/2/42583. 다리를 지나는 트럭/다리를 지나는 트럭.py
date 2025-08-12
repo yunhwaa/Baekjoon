a72291d65d9dcef455d54a78aad6f730b902c189
yunhwaa <140477502+yunhwaa@users.noreply.github.com>
2025-08-12 21:49:36 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 68.09 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,25 +1,21 @@
+from collections import deque
+
 def solution(bridge_length, weight, truck_weights):
     time = 0
-    passing_truck = [0] * bridge_length # 다리 건너는 트럭
-    sum_weight = 0
+    t_weight = 0
+    truck = deque(truck_weights)
+    bridge = deque([0] * bridge_length)
     
-    while len(truck_weights) > 0:
+    while truck or t_weight > 0:
         time += 1
-        sum_weight -= passing_truck.pop(0)
+        t = bridge.popleft()
+        t_weight -= t
         
-        if sum_weight + truck_weights[0] <= weight:
-            sum_weight += truck_weights[0]
-            passing_truck.append(truck_weights.pop(0))
+        if truck and t_weight + truck[0] <= weight:
+            t = truck.popleft()
+            bridge.append(t)
+            t_weight += t
         else:
-            passing_truck.append(0)
-    
-    time += bridge_length
+            bridge.append(0)       
     return time
-# 대기 트럭에서 맨 앞에꺼 빼서 다리 건너는 트럭(passing_truck)에 넣어
-# 다리 건너는 트럭이 bridge_length대보다 작고, weight보다 작으면 
-# time에 bridge 개수만큼 더하기 
-# 다리를 지난 트럭이 대기 트럭에 있는 수와 똑같으면 time 리턴 
 
-# bridge_length: 다리에 올라갈 수 있는 트럭 개수(최대 트럭 수)
-# weight: 한 다리에 올라갈 수 있는 무게
-# truck_weights: 트럭 별 무게 
