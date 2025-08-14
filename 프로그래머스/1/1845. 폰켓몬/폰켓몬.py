e991ec4802162573c15b9a630e34ead639c63dc9
yunhwaa <140477502+yunhwaa@users.noreply.github.com>
2025-08-14 19:06:15 +0900

    [level 1] Title: 폰켓몬, Time: 0.72 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,13 +1,5 @@
 def solution(nums):
-    hash_set = set()  # 고유 포켓몬 종류 저장
-    
-    for num in nums:
-        hash_set.add(num)  # 중복 없이 저장됨
-        
-    select_count = len(nums) // 2  # 고를 수 있는 수: 전체 포켓몬 수의 절반
-    return min(len(hash_set), select_count)
-    
-    # return min(len(set(nums)), len(nums)//2)
-
-
-
+    answer = 0
+    n = len(nums) // 2
+    answer = min(len(set(nums)), n)
+    return answer
