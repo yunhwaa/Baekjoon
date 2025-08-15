01b0b69b6237ac05c799dd39b89dfe8d98f631fe
yunhwaa <140477502+yunhwaa@users.noreply.github.com>
2025-08-15 20:38:35 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,16 +1,13 @@
 def solution(clothes):
     answer = 1
-    closet = {}
-    
-    for i in clothes:
-        category = i[1]
-        if category in closet:
-            closet[category] += 1
+    clo_dict = {}
+    for c in clothes:
+        if c[1] not in clo_dict.keys():
+            clo_dict[c[1]] = 1
         else:
-            closet[category] = 1
-    
-    for n in closet.values():
-        answer *= (n + 1)  # 안 입는 경우도 고려!
-
-    
-    return answer - 1    # 모두 안 입는 경우 제외
+            clo_dict[c[1]] += 1
+    num = list(clo_dict.values())
+    for n in num:
+        answer *= (n+1) 
+    answer -= 1
+    return answer
