cbb6f2b396b1ac9596220fb36b0406239af26c9c
yunhwaa <140477502+yunhwaa@users.noreply.github.com>
2025-08-15 21:28:39 +0900

    [level 3] Title: 베스트앨범, Time: 0.12 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,29 +1,22 @@
 def solution(genres, plays):
     answer = []
-    
-    playlist = {}
-    # 장르 선택 
+    gen_dict = {}
     for g, p in zip(genres, plays):
-        if g in playlist:
-            playlist[g] += p
+        if g not in gen_dict.keys():
+            gen_dict[g] = p
         else:
-            playlist[g] = p
-    
-    sorted_playlist = dict(sorted(playlist.items(), key=lambda x: x[1], reverse=True))   # 내림차순 정렬
-    
-    genres_list = list(sorted_playlist.keys())
-    
-    for i in range(len(genres_list)):
-        plays_num = {}
-        n = 0
-        for g in range(len(genres)):
-            if genres_list[i] == genres[g]:
-                plays_num[g] = plays[g]
-        # plays_num: {고유번호: play재생횟수}
-        # 재생횟수 많고(내림차순 정렬), 고유번호 작을수록(오름차순 정렬)
-        sorted_plays_num = sorted(plays_num.items(), key=lambda x: (-x[1], x[0]))
-        
-        for n in range(min(2, len(sorted_plays_num))):
-            answer.append(sorted_plays_num[n][0])
+            gen_dict[g] += p
             
+    gen_dict = sorted(gen_dict.items(), key = lambda item: item[1], reverse=True)     
+    
+    for k, y in gen_dict:
+        ans_list = []
+        for i, (g, p) in enumerate(zip(genres, plays)):
+            if k == g:
+                ans_list.append([p, i])
+        ans_list = sorted(ans_list, key = lambda x: [-x[0], x[1]])
+        answer.append(ans_list[0][1])
+        if len(ans_list) > 1:
+            answer.append(ans_list[1][1])
+
     return answer
