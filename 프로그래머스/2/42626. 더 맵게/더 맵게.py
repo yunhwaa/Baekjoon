474951e93607f9f6e5c80f072f3849e0b850ad42
yunhwaa <140477502+yunhwaa@users.noreply.github.com>
2025-08-17 16:41:37 +0900

    [level 2] Title: 더 맵게, Time: 1799.26 ms, Memory: 48.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,16 +1,19 @@
-import heapq
-
+import heapq 
+heapq.heapify
 def solution(scoville, K):
-    cnt = 0
-    heapq.heapify(scoville)  # 리스트를 heap으로 변환
-
-    while len(scoville) >= 2 and scoville[0] < K:
-        first_n = heapq.heappop(scoville)
-        second_n = heapq.heappop(scoville)
-        
-        heapq.heappush(scoville, first_n + second_n * 2)
-        cnt += 1
     
-    # 모든 음식을 다 섞고도 K이상이 안 되는 경우 존재
-    # ex) solution([1, 1], 100)  -> 모든 걸 섞어도 100을 못 넘음 -> 1
-    return cnt if scoville[0] >= K else -1 
+    answer = 0
+    heapq.heapify(scoville)
+    while True:
+        
+        if scoville[0] >= K:
+            break
+        if len(scoville) == 1:
+            return -1 
+            
+        num1, num2 = heapq.heappop(scoville), heapq.heappop(scoville)
+        num = num1 + (num2 * 2) 
+        heapq.heappush(scoville, num)
+        answer += 1
+        
+    return answer
