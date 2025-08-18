e5c49ebdb0ed05c8e2c67ba7075cd7457ed4394c
yunhwaa <140477502+yunhwaa@users.noreply.github.com>
2025-08-19 00:40:58 +0900

    [level 3] Title: 이중우선순위큐, Time: 238.73 ms, Memory: 15.5 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,33 +1,30 @@
-import heapq
+import heapq 
 
 def solution(operations):
     heap = []
-    
+    answer = []
     for num in operations:
-        n, m = num.split(" ")
+        n, m = num.split(' ')
         m = int(m)
         
         if n == "I":
-            heapq.heappush(heap, m)  # 작은 수 맨 앞에 
-            
+            heapq.heappush(heap, m)
         elif n == "D":
             if not heap:
                 continue
-                
             if m == 1:
                 heap = sorted(heap)
-                heap.pop()
-                heapq.heapify(heap)
-                
+                heap.remove(heap[-1])
             elif m == -1:
                 heapq.heappop(heap)
-    
-          
-            
+                
     if len(heap) == 0:
-        heap = [0, 0]
+        answer.append(0)
+        answer.append(0)
     else:
         heap = sorted(heap)
-        return (heap[-1], heap[0])
-        
-    return heap
+        answer.append(heap[-1])
+        answer.append(heap[0])
+    
+    
+    return answer
