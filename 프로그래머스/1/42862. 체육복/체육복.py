f474a12dea35e7cf735f48fa5cd364fc9c23af52
yunhwaa <140477502+yunhwaa@users.noreply.github.com>
2025-09-08 21:48:56 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,34 +1,16 @@
-# reserve와 lost 비교
-# -> lost 순차 탐색 (for문 사용)
-# -> lost[i] 번호 +-1에 해당하는 번호가 reserve에 있으면 
-#  count += 1
-# 최종 answer: n - len(lost) + count
-
-# 이거 젤 먼저! 
-# reserve에 있는 학생이 lost에도 있으면 빌려줄 수 없음
-# reserve에서 제거 
-# 이 말의 의미! -> 자기 체육복을 도난당했지만 여벌이 있어서 입을 수 있음을 의미 
 def solution(n, lost, reserve):
-    answer = n - len(lost)
-    count = 0
+    answer = 0
+    lost_set = set(lost)
+    reserve_set = set(reserve)
     
-    lost.sort()
-    reserve.sort()
+    overlap = lost_set & reserve_set
+    lost_set -= overlap
+    reserve_set -= overlap
     
-    new_lost = []
-    for l in lost:
-        if l in reserve:
-            reserve.remove(l)
-            answer += 1  
-        else:
-            new_lost.append(l)
-    
-    for l in new_lost:
-        if (l - 1) in reserve:
-            reserve.remove(l - 1)
-            count += 1
-        elif (l + 1) in reserve:
-            reserve.remove(l + 1)
-            count += 1
-    answer += count
+    for r in sorted(reserve_set):
+        if r-1 in lost_set:
+            lost_set.remove(r-1)
+        elif r+1 in lost_set:
+            lost_set.remove(r+1)
+    answer = n - len(lost_set)
     return answer
